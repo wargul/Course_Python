342a667253ed57a08b858045a8c878b7d3093927
alex <alexxivf@gmail.com>
2019-10-03 20:25:21 +0300

    FirstCoursePyt.py

--- FirstCoursePyt.py
+++ FirstCoursePyt.py
@@ -1,1 +1,19 @@
-2
+#height = int(input("Enter height .."))
+#for i in range(height):
+ #   for k in range(i + 1):
+  #      print("#", end="")
+   # print()
+
+#height = int(input("Enter height .."))
+#for i in range(height):
+ #   for k in range(height - i):
+#        print("#", end="")
+ #   print()
+
+height = int(input("Enter height .."))
+for i in range(height):
+    for z in range(height - i):
+        print(" ", end="")
+    for k in range(i):
+        print("#", end="")
+    print()
