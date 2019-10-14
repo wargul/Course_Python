837a3c362a5ce7b129f3cc03175ab4530bcb05ff
alex <alexxivf@gmail.com>
2019-10-14 16:46:34 +0300

    Polindrom

---
+++ 4.2HomePalyndrom.py
@@ -0,0 +1,8 @@
+s_pal = input("Please tape your word : ...")
+
+#s_lap = s_pal[::-1]
+if s_pal == s_pal[::-1]:
+    print("The Polyndrom word is correct ", s_pal)
+else: print("wrong")
+
+# if s_pal ==
