2f2246a86e89aef485e0a543aa87d1997bc2f6c5
alex <alexxivf@gmail.com>
2019-10-22 20:41:31 +0300

    List_19_10_2019

---
+++ 6Less_List_Pyt.py
@@ -0,0 +1,28 @@
+weekdays = []
+print(weekdays)
+weekdays = ["Monday", "Tuesday", "Wednesday"]
+print(weekdays)
+#first item in the list
+print(weekdays[0])
+print(weekdays[0:2])
+
+another_list = list()
+print(another_list)
+
+
+cat = list("cat")
+print(cat)
+
+cat2 = ["cat"]
+print(cat2)
+
+cat3 = ["cat", "dog"]
+print(cat3)
+
+#wrong
+#cat4 = list("cat", "dog")
+#print(cat4)
+
+#magic
+cat5 = list(("cat", "dog"))
+print(cat5)
